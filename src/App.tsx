5dbbc6942b8d3994397fb097c8f1f9c3b496bfc8
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2023-06-05 16:15:21 +0330

    wrap VendorsListPage with Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import VendorsListPage from "./modules/vendors-list/view";
+import { Provider } from "react-redux";
+import { store } from "./shared/services/store";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-    </>
+    <Provider store={store}>
+      <VendorsListPage />
+    </Provider>
   );
 }
 
